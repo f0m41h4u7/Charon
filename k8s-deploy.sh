ed5e602de72c1764eb0342d8032a25733d193b05
f0m41h4u7 <fomalhaut_a3v@outlook.com>
2020-02-21 01:06:41 +0300

    cleaned repo

--- k8s-deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-kubectl create -f deploy/service_account.yaml
-kubectl create -f deploy/role.yaml
-kubectl create -f deploy/role_binding.yaml
-kubectl create -f deploy/operator.yaml
-kubectl create -f deploy/crds/charon.charon.cr_charons_crd.yaml
