5e221f520873b38f0b4af53d3fb3627fb91afa17
f0m41h4u7 <fomalhaut_a3v@outlook.com>
2020-03-06 20:22:03 +0000

    start implementing analyzer

--- analyzer/get_prom_metrics.py
+++ analyzer/get_prom_metrics.py
@@ -1,15 +1,15 @@
 from prometheus_api_client import Metric, MetricsList, PrometheusConnect
 from prometheus_api_client.utils import parse_datetime, parse_timedelta
-
-import matplotlib.pyplot as plt
+import pandas as pd
 
 pc = PrometheusConnect(url="http://167.172.137.177:30329", disable_ssl=True)
 
-start_time = parse_datetime("3d")
+start_time = parse_datetime("1h")
 end_time = parse_datetime("now")
-chunk_size = parse_timedelta("now", "1d")
+chunk_size = parse_timedelta("now", "1h")
 
 all = pc.all_metrics()
+df = pd.DataFrame()
 for metric_type in all:
 	metric_data = pc.get_metric_range_data(
 		metric_type,
@@ -19,6 +19,5 @@ for metric_type in all:
 	)
 	metrics_object_list = MetricsList(metric_data)
 	for item in metrics_object_list:
-		print(item.metric_name, item.label_config)
-		print(item.metric_values)
-	print("\n ******************** \n")
+		df[item.metric_name] = item.metric_values['y']
+print(df)
