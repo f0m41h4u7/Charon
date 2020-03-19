4cf6189828da04c65e41c048422aef974e488e9b
f0m41h4u7 <fomalhaut_a3v@outlook.com>
2020-03-19 19:23:57 +0000

    add README and fix style

---
+++ README.md
@@ -0,0 +1,5 @@
+## Charon
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+Smart rollout/rollback system for K8s
--- analyzer/get_prom_metrics.py
+++ analyzer/get_prom_metrics.py
@@ -3,21 +3,24 @@ from prometheus_api_client.utils import parse_datetime, parse_timedelta
 import pandas as pd
 import numpy as np
 
+
 def calc_delta(vals):
-	diff = vals - np.roll(vals, 1)
-	diff[0] = 0
-	return diff
+    diff = vals - np.roll(vals, 1)
+    diff[0] = 0
+    return diff
+
 
 def monotonically_inc(vals):
-	if len(vals) == 1:
-		return True
-	diff = calc_delta(vals)
-	diff[np.where(vals == 0)] = 0
+    if len(vals) == 1:
+        return True
+    diff = calc_delta(vals)
+    diff[np.where(vals == 0)] = 0
 
-	if ((diff < 0).sum() == 0):
-		return True
-	else:
-		return False
+    if (diff < 0).sum() == 0:
+        return True
+    else:
+        return False
+
 
 def get_metrics():
     pc = PrometheusConnect(url="http:/prometheus-service:9090", disable_ssl=True)
@@ -29,19 +32,16 @@ def get_metrics():
     metric_type = "testMetrics"
 
     metric_data = pc.get_metric_range_data(
-	metric_type,
-	start_time=start_time,
-	end_time=end_time,
-	chunk_size=chunk_size,
+        metric_type, start_time=start_time, end_time=end_time, chunk_size=chunk_size,
     )
-    
+
     metrics_object_list = MetricsList(metric_data)
     df = pd.DataFrame()
     for item in metrics_object_list:
-        vals = np.array(item.metric_values['y'].tolist())
+        vals = np.array(item.metric_values["y"].tolist())
         if monotonically_inc(vals):
             vals = calc_delta(vals)
-        df['ds'] = item.metric_values['ds']
-        df['y'] = vals
+        df["ds"] = item.metric_values["ds"]
+        df["y"] = vals
 
     return df
