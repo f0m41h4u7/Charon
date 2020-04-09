2aa4ee382a5a8bea9b66a49179aca729d4b9add4
f0m41h4u7 <fomalhaut_a3v@outlook.com>
2020-04-09 13:10:57 +0000

    add setup.sh

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cp .env main/
+cp .env analyzer/
+
+kubectl apply -f charon-operator/deploy/service_account.yaml
+kubectl apply -f charon-operator/deploy/role.yaml
+kubectl apply -f charon-operator/deploy/role_binding.yaml
+kubectl apply -f charon-operator/deploy/operator.yaml
+kubectl apply -f charon-operator/deploy/crds/app.custom.cr_apps_crd.yaml
+kubectl apply -f charon-operator/deploy/crds/deployer.charon.cr_deployers_crd.yaml
+kubectl apply -f main/deployer.yaml
